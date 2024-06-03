a18368144c113aca5bf2685f250508875dfdb915
Bac Truong Van <bactruongvan17@gmail.com>
2024-06-03 22:21:05 +0700

    Update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,11 +10,24 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "tokyonight",
+      },
+    },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -27,7 +40,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  -- install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  -- "craftzdog/solarized-osaka.nvim",
+  -- lazy = true,
+  -- priority = 1000,
+  -- opts = function()
+  --   return {
+  --     transparent = true,
+  --   }
+  -- end,
+  "folke/tokyonight.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+      style = "moon",
+    }
+  end,
+}
